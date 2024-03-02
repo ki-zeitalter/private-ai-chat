01c286a4c8e8b25f6eb4381b33c8bb6a6fd27d64
sgruenheit <styrmir.gruenheit@bridging-it.de>
2024-03-02 08:16:31 +0100

    Created initial layout including the chat component

--- webclient/src/app/app.component.ts
+++ webclient/src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+import {AsyncPipe} from "@angular/common";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatButtonModule, MatIconButton} from "@angular/material/button";
+import {MatListItem, MatListModule, MatNavList} from "@angular/material/list";
+import {MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {Observable} from "rxjs";
+import {map, shareReplay} from "rxjs/operators";
+
+import 'deep-chat'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    RouterOutlet,
+    AsyncPipe,
+    MatIcon,
+    MatIconButton,
+    MatListItem,
+    MatNavList,
+    MatSidenav,
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatToolbar,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'webclient';
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
