2f6ac4c03fc159fb52a455c6f6b252792b34e133
sgruenheit <styrmir.gruenheit@bridging-it.de>
2024-03-06 20:41:32 +0100

    History is now sorted by timestamp

--- server/services/history_inmemory_repository.py
+++ server/services/history_inmemory_repository.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class HistoryInMemoryRepository:
     def __init__(self):
         self._history = []
@@ -7,11 +10,17 @@ class HistoryInMemoryRepository:
 
         if index_of_existing is not None:
             self._history[index_of_existing]["messages"] = messages
+            self._history[index_of_existing]["timestamp"] = datetime.now().isoformat()
         else:
-            self._history.append({"user_id": user_id, "thread_id": thread_id, "messages": messages})
+            self._history.append({"user_id": user_id,
+                                  "thread_id": thread_id,
+                                  "messages": messages,
+                                  "timestamp": datetime.now().isoformat()})
 
     def get_history(self, user_id):
-        return [item for item in self._history if item["user_id"] == user_id]
+        user_history = [item for item in self._history if item["user_id"] == user_id]
+        sorted_history = sorted(user_history, key=lambda x: x['timestamp'], reverse=True)
+        return sorted_history
 
     def get_history_thread(self, user_id, thread_id):
         return [item["messages"] for item in self._history if
