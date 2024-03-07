8a3264da3b4e5567df1ce6594be3ac4b4aaa711f
sgruenheit <styrmir.gruenheit@bridging-it.de>
2024-03-07 16:22:57 +0100

    Displaying a logo

--- webclient/src/app/components/menu/menu.component.ts
+++ webclient/src/app/components/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {AsyncPipe, NgForOf} from "@angular/common";
+import {AsyncPipe, NgForOf, NgOptimizedImage} from "@angular/common";
 import {ChatComponent} from "../chat/chat.component";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
@@ -40,7 +40,8 @@ import {RouterOutlet} from "@angular/router";
     MatSidenavContent,
     MatToolbar,
     NgForOf,
-    RouterOutlet
+    RouterOutlet,
+    NgOptimizedImage
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
