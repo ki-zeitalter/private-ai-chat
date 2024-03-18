398a58c5fab35e58a1c9a65593d8272dc3b96901
sgruenheit <styrmir.gruenheit@bridging-it.de>
2024-03-18 16:51:05 +0100

    Implementing editor for assistants (wip)

--- webclient/src/app/components/assistant-editor/assistant-editor.component.ts
+++ webclient/src/app/components/assistant-editor/assistant-editor.component.ts
@@ -5,10 +5,14 @@ import {MatOption, MatSelect} from "@angular/material/select";
 import {MatSlideToggle} from "@angular/material/slide-toggle";
 import {MatDivider} from "@angular/material/divider";
 import {MatIcon} from "@angular/material/icon";
-import {MatMiniFabButton} from "@angular/material/button";
+import {MatButton, MatMiniFabButton} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
 import {MatList, MatListItem, MatListOption, MatSelectionList} from "@angular/material/list";
 import {AICard} from "../../model/ai-card.model";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {v4 as uuidv4} from "uuid";
 
 @Component({
   selector: 'app-assistant-editor',
@@ -27,19 +31,50 @@ import {AICard} from "../../model/ai-card.model";
     MatList,
     MatListItem,
     MatSelectionList,
-    MatListOption
+    MatListOption,
+    FormsModule,
+    NgForOf,
+    MatButton,
+    ReactiveFormsModule
   ],
   templateUrl: './assistant-editor.component.html',
   styleUrl: './assistant-editor.component.scss'
 })
 export class AssistantEditorComponent {
-  assistantData: AICard = {
-    name: '',
-    description: '',
-    type: '',
-    agent_id: '',
-    creator: '',
-    instructions: '',
-    tools: []
-  };
+
+  formular = new FormGroup({
+    name: new FormControl('', Validators.required),
+    instructions: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    model: new FormControl(''),
+    codeInterpreter: new FormControl(false),
+    retrieval: new FormControl(false),
+  });
+
+
+  // TODO functions
+
+  files: any[] = [];
+
+  save(): void {
+    const tools = [];
+
+    if (this.formular.get('codeInterpreter')?.value) {
+      tools.push('{"type": "code_interpreter"}');
+    }
+    if (this.formular.get('retrieval')?.value) {
+      tools.push('{"type": "retrieval"}');
+    }
+
+    const assistantData: AICard = {
+      name: this.formular.get('name')!.value!,
+      description: this.formular.get('description')!.value!,
+      type: 'assistant',
+      agent_id: uuidv4(),
+      creator: 'manual',
+      instructions: this.formular.get('instructions')!.value!,
+      tools: tools
+    };
+    console.log(assistantData);
+  }
 }
